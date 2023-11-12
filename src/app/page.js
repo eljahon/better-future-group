3bd34b295d69ba4bc35df9a7701d48d56d4ac3c2
Ogabek <mustafoev0806@gmail.com>
2023-11-12 22:12:07 +0500

    added: sticky navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-
 import Navbar from "@/components/Navbar";
 import GlobalProblems from "@/components/GlobalProblems";
+
 export default function Home() {
   return (
-    <main className='container'>
-          <Navbar/>
-        <GlobalProblems/>
+    <main className="container">
+      <Navbar />
+      <GlobalProblems />
     </main>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,53 @@
-import {Fragment} from "react";
-import Image from 'next/image'
-import logo from '../assets/images/logo.png'
-export default  function Navbar () {
-    const list = [{name: 'Home', id: 1,},{name: 'Our products', id: 2},{name: 'Our projects', id: 3},{name: 'About us', id: 4},{name: 'Our team', id: 5},{name: 'Contact us', id: 5}]
-    return<div>
-    <div className='flex justify-between py-5'>
-        <div><Image src={logo}/></div>
+"use client";
+
+import { Fragment, useEffect, useState } from "react";
+import Image from "next/image";
+import logo from "../assets/images/logo.png";
+
+export default function Navbar() {
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  const list = [
+    { name: "Home", id: 1 },
+    { name: "Our products", id: 2 },
+    { name: "Our projects", id: 3 },
+    { name: "About us", id: 4 },
+    { name: "Our team", id: 5 },
+    { name: "Contact us", id: 6 },
+  ];
+
+  const getWindowHeight = () => {
+    setWindowHeight(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", getWindowHeight);
+    return () => window.removeEventListener("scroll", getWindowHeight);
+  }, [window.scrollY]);
+
+  return (
+    <div
+      className={`${
+        windowHeight > 0 && " shadow-xl"
+      } fixed bg-white top-0 left-0 w-full`}
+    >
+      <div className={`${windowHeight>0?"py-3":"py-5"} transition-all duration-300 flex container justify-between items-center`}>
+        <div>
+          <Image src={logo} />
+        </div>
         <nav>
-       <ul className='flex'>
-           {list.map((el) => (<li className='ml-8 lg:text-base md:text-sm font-medium' key={el.id}><a href='#'>{el.name}</a></li>))}
-       </ul>
+          <ul className="flex">
+            {list.map((el) => (
+              <li
+                className="ml-8 lg:text-base md:text-sm font-medium"
+                key={el.id}
+              >
+                <a href="#">{el.name}</a>
+              </li>
+            ))}
+          </ul>
         </nav>
+      </div>
     </div>
-    </div>
+  );
 }
